f2cebff6e1e2d86141b4adfd54a259c74f18bea0
Bhakta Santos <bhaktakrpa@gmail.com>
2022-04-29 16:19:26 -0300

    Updated Styles

--- src/views/ViewView.vue
+++ src/views/ViewView.vue
@@ -1,108 +1,110 @@
-<template><div>
-    <div class="sfooter desk">
+<template>
+    <div>
+        <div class="sfooter desk">
 
-        Solsunsets&copy; 2022 | Powered by <a alt="home" href="https://solsunsets.com">
-            <object href="https://solsunsets.com" id="svg1" data="https://solsunsets.com/img/logo/logo-sln.svg" class="logo-sln"></object>
-        </a>
-    </div>
-
-
-    <div class="blog">
-
-        <div class="logo">
-            <a href="https://solsunsets.com">
-                <object href="https://solsunsets.com" id="svg1" data="https://solsunsets.com/img/logo/logo-ss-m.svg" class="logo-ss"></object>
+            Solsunsets&copy; 2022 | Powered by <a alt="home" href="https://solsunsets.com">
+                <object href="https://solsunsets.com" id="svg1" data="https://solsunsets.com/img/logo/logo-sln.svg" class="logo-sln"></object>
             </a>
         </div>
-        <div class="blog-header-container">
-            <div class="blog-intro desk">
-                <div class="logo-links">
-
-                    <a alt="home" href="https://solsunsets.com">
-                        <object href="https://solsunsets.com" id="svg1" data="https://solsunsets.com/img/logo/home.svg" class="logo-twitter mob"></object>
-                    </a>
-                    <a href="https://twitter.com/solsunsets">
-                        <object href="https://twitter.com/solsunsets" id="svg1" data="https://solsunsets.com/img/logo/logo-ttr.svg" class="logo-twitter"></object>
-                    </a><a href="https://discord.gg/RNSRHaxzbR">
-                        <object href="https://discord.gg/RNSRHaxzbR" id="svg1" data="https://solsunsets.com/img/logo/logo-dsc.svg" class="logo-discord"></object>
-                    </a><a href="https://docs.solsunsets.com">
-                        <object href="https://docs.solsunsets.com" id="svg1" data="https://solsunsets.com/img/logo/logo-gtb.svg" class="logo-discord"></object>
-                    </a>
-                    <a href="https://www.magiceden.io/marketplace/solsunsets">
-                        <object href="https://www.magiceden.io/marketplace/solsunsets" id="svg1" data="https://solsunsets.com/img/logo/logo-me.svg" class="logo-discord"></object>
-                    </a>
 
 
-                </div>
-                <span>
-                    Solsunsets is an Art Collection of 4,444 generative landscapes inspired by the aesthetics of popular culture, nostalgic childhood memories, obsolete technologies and jazz fusion from the 80s. From the 1,33B possible combinations between the 112 handcrafted attributes (skies, suns, clouds, grounds, horizons and objects), only 4,444 striking and unique art pieces were generated. A project created by <a href="https://twitter.com/rgb0x00">@rgb0x00<object id="svg1" data="/img/logo/open.svg" style="height:1em;margin-bottom:-0.2em;"></object></a>
-                </span>
+        <div class="blog">
 
+            <div class="logo">
+                <a href="https://solsunsets.com">
+                    <object href="https://solsunsets.com" id="svg1" data="https://solsunsets.com/img/logo/logo-ss-m.svg" class="logo-ss"></object>
+                </a>
             </div>
+            <div class="blog-header-container">
+                <div class="blog-intro desk">
+                    <div class="logo-links">
 
-        </div>
-        <div class="blog-part">
-
-            <div class="blog-right">
-                <div class="intro">
-                    <div class="mob">
-                        <a class="nav-link" href="https://solsunsets.com">
-                            <b>
-                                <object href="https://solsunsets.com" id="svg1" data="https://solsunsets.com/img/logo/arrow.svg" class="logo-rotate"></object>HOME
-                            </b>
+                        <a alt="home" href="https://solsunsets.com">
+                            <object href="https://solsunsets.com" id="svg1" data="https://solsunsets.com/img/logo/home.svg" class="logo-twitter mob"></object>
+                        </a>
+                        <a href="https://twitter.com/solsunsets">
+                            <object href="https://twitter.com/solsunsets" id="svg1" data="https://solsunsets.com/img/logo/logo-ttr.svg" class="logo-twitter"></object>
+                        </a><a href="https://discord.gg/RNSRHaxzbR">
+                            <object href="https://discord.gg/RNSRHaxzbR" id="svg1" data="https://solsunsets.com/img/logo/logo-dsc.svg" class="logo-discord"></object>
+                        </a><a href="https://docs.solsunsets.com">
+                            <object href="https://docs.solsunsets.com" id="svg1" data="https://solsunsets.com/img/logo/logo-gtb.svg" class="logo-discord"></object>
+                        </a>
+                        <a href="https://www.magiceden.io/marketplace/solsunsets">
+                            <object href="https://www.magiceden.io/marketplace/solsunsets" id="svg1" data="https://solsunsets.com/img/logo/logo-me.svg" class="logo-discord"></object>
                         </a>
 
+
                     </div>
-                    <i>
-                    </i>
-                </div>
-                <!--all the config stuff-->
-                <div class="connect">
-                    <NFTViewForm :is-loading="isLoading" @submit-form="handleSubmitForm">
-                    </NFTViewForm>
+                    <span>
+                        Solsunsets is an Art Collection of 4,444 generative landscapes inspired by the aesthetics of popular culture, nostalgic childhood memories, obsolete technologies and jazz fusion from the 80s. From the 1,33B possible combinations between the 112 handcrafted attributes (skies, suns, clouds, grounds, horizons and objects), only 4,444 striking and unique art pieces were generated. A project created by <a href="https://twitter.com/rgb0x00">@rgb0x00<object id="svg1" data="/img/logo/open.svg" style="height:1em;margin-bottom:-0.2em;"></object></a>
+                    </span>
+
                 </div>
 
+            </div>
+            <div class="blog-part">
+
+                <div class="blog-right">
+                    <div class="intro">
+                        <h1>Solsunsets Banners</h1>
+                        <div class="mob">
+                            <a class="nav-link" href="https://solsunsets.com">
+                                <b>
+                                    <object href="https://solsunsets.com" id="svg1" data="https://solsunsets.com/img/logo/arrow.svg" class="logo-rotate"></object>HOME
+                                </b>
+                            </a>
+
+                        </div>
+                    </div>
+                    <div ><a class="cyan" href="https://gallery.solsunsets.com/"><span class="material-symbols-outlined">
+    arrow_back_ios
+</span><span class="back_gallery">Back to the gallery to access all your Banners</span></a></div>
+                    You can also view any Solsunset banner right through the
+                    https://gallery.solsunsets.com/view/mint/ adding the token/mint address at the end of the URL (it's the same token address from MagicEden item details).
+
+
+
+                    <!--all the config stuff-->
+                    <div class="connect">
+                        <NFTViewForm :is-loading="isLoading" @submit-form="handleSubmitForm">
+                        </NFTViewForm>
+                    </div>
+
+
+                </div>
 
             </div>
-
-        </div>
-        <div id="menu" class="blog-part desk">
-
-            <div class="blog-right-title-container"></div>
-            <div class="bbutton"><a class="cyan" href="https://gallery.solsunsets.com/">SOLSUNSETS BANNERS</a></div>
-            <div class="bbutton desk"><a class="cyan" href="https://solsunsets.com/memorial">SOLSUNSETS MEMORIAL</a></div>
-            <div class="bbutton"><a class="cyan" href="https://up.solsunsets.com/">PIXEL ART UPSCALE TOOL</a></div>
-            <div class="bbutton">MARKETS: <a class="cyan" href="https://www.magiceden.io/marketplace/solsunsets">MAGICEDEN<object id="svg1" data="https://solsunsets.com/img/logo/open.svg" style="height:1em;margin-bottom:-0.2em;"></object></a> | <a href="https://alpha.art/collection/solsunsets/">ALPHA.ART<object id="svg1" data="https://solsunsets.com/img/logo/open.svg" style="height:1em;margin-bottom:-0.2em;"></object></a></div>
-            <div class="bbutton desk">
-                AUCTIONS SERIES:
-                <a class="cyan" href="https://suns.holaplex.com/listings?view=ended">HOLAPLEX <object id="svg1" data="https://solsunsets.com/img/logo/open.svg" style="height:1em;margin-bottom:-0.2em;"></object></a>
+            <div id="menu" class="blog-part desk">
+                <div class="blog-right-title-container"></div>
+                <div class="bbutton"><a class="cyan" href="https://gallery.solsunsets.com/">Solsunsets Banners</a></div>
+                <div class="bbutton desk"><a class="cyan" href="/">Pixels Claim (soon)</a></div>
+                <div class="bbutton desk"><a class="cyan" href="https://up.solsunsets.com/">Pixel Art Upscale Tool</a></div>
+                <div class="bbutton">Markets: <a class="cyan" href="https://www.magiceden.io/marketplace/solsunsets">MagicEden<object id="svg1" data="https://solsunsets.com/img/logo/open.svg" style="height:1em;margin-bottom:-0.2em;"></object></a> | <a href="https://alpha.art/collection/solsunsets/">Alpha.Art<object id="svg1" data="https://solsunsets.com/img/logo/open.svg" style="height:1em;margin-bottom:-0.2em;"></object></a></div>
+                <div class="bbutton desk">Sci-fi Series:<a class="cyan" href="https://exchange.art/collections/Solsunsets%20Sci-Fi%20Series">Exchange.Art<object id="svg1" data="https://solsunsets.com/img/logo/open.svg" style="height:1em;margin-bottom:-0.2em;"></object></a></div>
+                <div class="bbutton desk">Rarity: <a class="cyan" href="https://moonrank.app/collection/solsunsets">Moonrank<object id="svg1" data="https://solsunsets.com/img/logo/open.svg" style="height:1em;margin-bottom:-0.2em;"></object></a> | <a class="cyan" href="https://howrare.is/solsunsets">Howrare<object id="svg1" data="https://solsunsets.com/img/logo/open.svg" style="height:1em;margin-bottom:-0.2em;"></object></a>  </div>
             </div>
 
-            <div class="bbutton desk">RARITY: <a class="cyan" href="https://moonrank.app/collection/solsunsets">MOONRANK<object id="svg1" data="https://solsunsets.com/img/logo/open.svg" style="height:1em;margin-bottom:-0.2em;"></object></a> | <a class="cyan" href="https://howrare.is/solsunsets">HOWRARE<object id="svg1" data="https://solsunsets.com/img/logo/open.svg" style="height:1em;margin-bottom:-0.2em;"></object></a>  </div>
 
 
         </div>
 
+        <!--per NFT display-->
+        <LoadingBar v-if="isLoading" :progress="progress" :text="text" class="my-5" />
+        <NotifyError v-else-if="isError" class="mt-5">{{ text }}</NotifyError>
+        <div v-else>
+            <NFTViewCard v-for="n in NFTs" :key="n.mint" :n="n"></NFTViewCard>
+        </div>
 
-    </div>
-
-    <!--per NFT display-->
-    <LoadingBar v-if="isLoading" :progress="progress" :text="text" class="my-5" />
-    <NotifyError v-else-if="isError" class="mt-5">{{ text }}</NotifyError>
-    <div v-else>
-        <NFTViewCard v-for="n in NFTs" :key="n.mint" :n="n"></NFTViewCard>
-    </div>
-
-    <!--modals-->
-    <!--must sit at the very bottom-->
-    <div class="infinite-loading">
-        <infinite-loading style="color:black;" @infinite="infiniteHandler"
-                          :identifier="
+        <!--modals-->
+        <!--must sit at the very bottom-->
+        <div class="infinite-loading">
+            <infinite-loading style="color:black;" @infinite="infiniteHandler"
+                              :identifier="
         +new Date() //needs to be something thta dynamically updates, or won't work
       "
-                          spinner="spiral"></infinite-loading>
+                              spinner="spiral"></infinite-loading>
+        </div>
     </div>
-</div>
 </template>
 
 <script lang="ts">
@@ -289,7 +291,8 @@
     });
 </script>
 
-<style>-
+<style>
+    -
     /*temp hackaround...*/
     .infinite-status-prompt {
         @apply text-white !important;
